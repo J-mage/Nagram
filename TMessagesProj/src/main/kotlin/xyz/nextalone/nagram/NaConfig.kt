74ad18c89a7eb2d0b86c91b2e0d75eefc93930e8
People-11 <349504120p@gmail.com>
2025-01-12 04:57:33 +1100

    feat: Ignore Delete Messages Request

--- TMessagesProj/src/main/kotlin/xyz/nextalone/nagram/NaConfig.kt
+++ TMessagesProj/src/main/kotlin/xyz/nextalone/nagram/NaConfig.kt
@@ -670,6 +670,12 @@ object NaConfig {
             ConfigItem.configTypeBool,
             false
         )
+    val disableUpdateDeleteMessages =
+        addConfig(
+            "DisableUpdateDeleteMessages",
+            ConfigItem.configTypeBool,
+            false
+        )
 
     private fun addConfig(
         k: String,
